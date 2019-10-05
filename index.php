c84575fdb1cce50b549a63703b25f00bab6db84e
Mauro Ribeiro <mauroribeiro.ti@gmail.com>
2019-10-05 20:31:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 //Apenas um teste para subir um arquivo para meu diretório.
 var $nome = "Mauro de Oliveira Ribeiro";
+var $profissao = "Técnico em Informática";
 echo $nome;
+echo "Sua profissão é: ".$profissao;
+
